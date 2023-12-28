787c956c9e6c78bb95e05579eb3f4b9a1424401d
blyt1 <blyt1@st-andrews.ac.uk>
2023-12-28 16:14:13 +0000

    asap

--- eval_all_final.py
+++ eval_all_final.py
@@ -38,21 +38,41 @@ with open('pickled_datasets/dasa_har.pickle', 'rb') as file:
 with open('pickled_datasets/wisdm_har.pickle', 'rb') as file:
     wisdm_har_df = pickle.load(file)
 
+datasets = [
+    ("HHAR", hhar_df, hhar_har_df, 'acc', 'acc', {}),
+    ("MotionSense", motion_sense_df, motionsense_har_df, 'all', 'acc', {}),
+    ("PAMAP", pamap_df, pamap_har_df, 'acc', 'acc', {}),
+    ("HARTH", harth_df, harth_har_df, 'acc', 'acc', {}),
+    ("DASD", dasa_df, dasa_har_df, 'acc', 'acc', {'shift': 10}),
+    ("WISDM", wisdm_df, wisdm_har_df, 'acc', 'acc', {'step': 2})
+]
 
-print("HHAR")
-print(Evaluation1.eval_downstream_model(hhar_df, hhar_har_df, 'acc', 'acc'))
+results = []
 
-print("MotionSense")
-print(Evaluation1.eval_downstream_model(motion_sense_df, motionsense_har_df, 'all', 'acc'))
+for dataset_name, dataset, har_dataset, target_columns, label_column, additional_params in datasets:
+    result = Evaluation1.eval_downstream_model(dataset, har_dataset, target_columns, label_column, **additional_params)
+    results.append((dataset_name, result))
 
-print("PAMAP")
-print(Evaluation1.eval_downstream_model(pamap_df, pamap_har_df, 'acc', 'acc'))
+# Print results
+for dataset_name, result in results:
+    print(dataset_name)
+    print(result)
+    print()
 
-print("HARTH")
-print(Evaluation1.eval_downstream_model(harth_df, harth_har_df, 'acc', 'acc'))
+# print("HHAR")
+# print(Evaluation1.eval_downstream_model(hhar_df, hhar_har_df, 'acc', 'acc'))
 
-print("DASD")
-print(Evaluation1.eval_downstream_model(dasa_df, dasa_har_df, 'acc', 'acc', shift=10))
+# print("MotionSense")
+# print(Evaluation1.eval_downstream_model(motion_sense_df, motionsense_har_df, 'all', 'acc'))
 
-print("WISDM")
-print(Evaluation1.eval_downstream_model(wisdm_df, wisdm_har_df, 'acc', 'acc', step=2))
+# print("PAMAP")
+# print(Evaluation1.eval_downstream_model(pamap_df, pamap_har_df, 'acc', 'acc'))
+
+# print("HARTH")
+# print(Evaluation1.eval_downstream_model(harth_df, harth_har_df, 'acc', 'acc'))
+
+# print("DASD")
+# print(Evaluation1.eval_downstream_model(dasa_df, dasa_har_df, 'acc', 'acc', shift=10))
+
+# print("WISDM")
+# print(Evaluation1.eval_downstream_model(wisdm_df, wisdm_har_df, 'acc', 'acc', step=2))
